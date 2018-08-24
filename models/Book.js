c83a2a2e4fe8bc79c5368d8aade39f838df69e4f
Elyes Turki <elyestu@gmail.com>
2018-08-24 02:10:32 +0100

    Create Routes for Accessing Book Data via Restful API

--- models/Book.js
+++ models/Book.js
@@ -1,12 +1,46 @@
+var express = require('express');
+var router = express.Router();
 var mongoose = require('mongoose');
-var BookSchema = new mongoose.Schema({
-    isbn: String,
-    title: String,
-    author: String,
-    description: String,
-    published_year: String,
-    publisher: String,
-    updated_date: { type: Date, default: Date.now },
-  });
+var Book = require('../models/Book.js');
 
-  module.exports = mongoose.model('Book', BookSchema);
+/* GET ALL BOOKS */
+router.get('/', function(req, res, next) {
+  Book.find(function (err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+/* GET SINGLE BOOK BY ID */
+router.get('/:id', function(req, res, next) {
+  Book.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* SAVE BOOK */
+router.post('/', function(req, res, next) {
+  Book.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* UPDATE BOOK */
+router.put('/:id', function(req, res, next) {
+  Book.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE BOOK */
+router.delete('/:id', function(req, res, next) {
+  Book.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
